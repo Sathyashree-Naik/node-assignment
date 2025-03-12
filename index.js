2832350ece418a706963f654d39b49950a89b5ed
Sathyashree-Naik <sathyashreeagnes@gmail.com>
2025-03-12 16:14:27 +0530

    created file using asynx await and fs module

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// Function returning a promise
+const fs = require("fs");
+
+// Function to return the promise
 function displayMessage() {
     return new Promise((resolve, _reject) => {
         setTimeout(() => {
@@ -11,3 +13,16 @@ function displayMessage() {
 displayMessage()
     .then((message) => console.log(message))
     .catch((error) => console.error("Error:", error));
+
+// function to create a file
+function createFile() {
+    try {
+        const message = "This is a file created using fs module and async/await.";
+        fs.writeFileSync("message.txt", message, "utf8");
+        console.log("File created successfully!");
+    } catch (error) {
+        console.error("Error creating file:", error);
+    }
+}
+
+createFile();
